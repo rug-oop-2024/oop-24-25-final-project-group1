3bc83862b1f664466aa62431f130e95f9f7ff16f
Kate <m.c.da.costa.vieira@student.rug.nl>
2024-11-24 17:46:10 +0100

    fixing final styling errors

--- autoop/core/ml/model/regression/__init__.py
+++ autoop/core/ml/model/regression/__init__.py
@@ -1,5 +1,6 @@
 """
 This is the initialization file for the regression module.
 
-It is used to initialize the regression submodule of the autoop.core.ml.model package.
+It is used to initialize the regression submodule of
+the autoop.core.ml.model package.
 """
